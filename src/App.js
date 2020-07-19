3cedf1ceab5c29907f2ead12cc3c946f69579238
jSeastar <Jseastar92@gmail.com>
2020-07-20 02:33:45 +0900

    04-3 react, component complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food({name, picture}){
+  return (
+    <div>
+      <h1> I like {name} </h1>
+      <img src={picture} alt={name}/>
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    id: 1,
+    name: 'Kimchi',
+    image: 'https://www.thespruceeats.com/thmb/gqJ0irQmKyBlRujeZh_DtUg79o0=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/kimchi-dish-served-on-a-white-plate-173021127-588b8a945f9b5874ee55f535.jpg',
+  },
+  {
+    id: 2,
+    name: 'Samgyeopsal',
+    image: 'https://cdn.crowdpic.net/detail-thumb/thumb_d_DB5AFFBD26B41E4934528EB193AF9F49.jpg',
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
+        ))}
     </div>
   );
 }
